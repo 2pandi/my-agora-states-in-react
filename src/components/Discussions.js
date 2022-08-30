79861c9658ac4ae31b1a964519a22800ddd2511f
2pandi <coder.2pandi@gmail.com>
2022-08-30 19:30:35 +0900

    reset data

--- src/components/Discussions.js
+++ src/components/Discussions.js
@@ -1,12 +1,6 @@
 import Discussion from "./Discussion";
-import { useState } from "react";
 
 export const Discussions = () => {
-  let discussionData = () => {
-    return fetch(`http://localhost:4000/discussions/`)
-    .then(res => res.json())
-  };
-  const [data, setData] = useState(discussionData);
     return (
       <section className="discussion__wrapper">
         <ul className="discussions__container">
