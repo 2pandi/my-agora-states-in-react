6fda005dd4c87c6bc220213391307b73cd84298a
2pandi <coder.2pandi@gmail.com>
2022-08-31 07:17:45 +0900

    set data(useState, useEffect)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
-import Discussions from './components/Discussions';
-import Form from './components/Form';
+import { Discussions } from './components/Discussions';
+import { Form } from './components/Form';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [discussions, setDiscussions] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/discussions/')
+    .then(res => res.json())
+    .then(data => {
+      setDiscussions(data);
+    })
+  }, []);
+
   return (
     <div className="App">
       <Form />
-      <Discussions />
+      <Discussions discussions={discussions}/>
     </div>
   );
 }
